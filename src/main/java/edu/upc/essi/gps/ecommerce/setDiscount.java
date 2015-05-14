656ac2c469be01275b7f8e3da88bb71b69b411bd
GerardDuch <gerard.duch@est.fib.upc.edu>
2015-05-14 12:20:27 +0200

    classes setDiscount i setDiscountCollection acabades

--- src/main/java/edu/upc/essi/gps/ecommerce/setDiscount.java
+++ src/main/java/edu/upc/essi/gps/ecommerce/setDiscount.java
@@ -4,4 +4,30 @@ package edu.upc.essi.gps.ecommerce;
  * Created by GerardDuch on 14/05/15.
  */
 public class setDiscount {
+    private String setObj;
+    private float discount;
+
+    public setDiscount() {}
+
+    public setDiscount(String setObj, float discount) {
+        this.setObj = setObj;
+        this.discount = discount;
+    }
+
+    public float getDiscount() {
+
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+    public String getSetObj() {
+        return setObj;
+    }
+
+    public void setSetObj(String setObj) {
+        this.setObj = setObj;
+    }
 }
--- src/main/java/edu/upc/essi/gps/ecommerce/setDiscountCollection.java
+++ src/main/java/edu/upc/essi/gps/ecommerce/setDiscountCollection.java
@@ -11,4 +11,34 @@ public class setDiscountCollection {
     public setDiscountCollection() {
         this.setDiscountController = new ArrayList<setDiscount>();
     }
+
+    private boolean existsSetDiscount (String set) {
+        for (setDiscount sd : setDiscountController) {
+            if (sd.getSetObj().equals(set)) return true;
+        }
+        return false;
+    }
+
+    private setDiscount getSetDiscountFromSet (String set) {
+        setDiscount result = new setDiscount();
+        for (setDiscount sd : setDiscountController) {
+            if (sd.getSetObj().equals(set)) result = sd;
+        }
+        return result;
+    }
+
+    public void addSetDiscount(setDiscount sd) throws RuntimeException {
+        if (existsSetDiscount(sd.getSetObj()))
+            throw new IllegalArgumentException("Ja existeix un descompte per aquest conjunt de productes");
+        if (sd.getDiscount() <= 0 || sd.getDiscount() > 100)
+            throw new IllegalArgumentException("El descompte ha de ser més gran que 0 i més petit o igual que 100");
+        
+        setDiscountController.add(sd);
+    }
+
+    public float getSetDiscount (String set) {
+        if (!existsSetDiscount(set))
+            throw new IllegalArgumentException("No existeix cap descompte de pels productes de tipus" + set);
+        return getSetDiscountFromSet(set).getDiscount();
+    }
 }
