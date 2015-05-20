4f5b01672bba405ae5c5a40a5d7d2459f9462c64
David Guerrero <david.guerrero@est.fib.upc.edu>
2015-05-20 21:52:08 +0200

    clase historial feta de nou

--- src/test/java/edu/upc/essi/gps/ecommerce/StepDefinitions.java
+++ src/test/java/edu/upc/essi/gps/ecommerce/StepDefinitions.java
@@ -366,24 +366,4 @@ public class StepDefinitions {
             this.posController.endSale();
     }
 
-    @Quan("^el gestor (.*) visualitza les ventes en una data (.*)$")
-    public void visualitzarPerData(String gestor, String data) throws Throwable {
-        tryCatch(() -> this.posController.visualitzarPerData(gestor, data));
-    }
-
-    @Aleshores("^el sistema mostra la venta de (\\d+)€ feta per en (.*)$")
-    public void comprovaVentaPerDia(int totalPrice, String assistant){
-        Map<String, TreeMap> salesPerData = new TreeMap<String, TreeMap>();
-        String data = this.posController.getCurrentDate();
-        salesPerData = this.posController.visualitzarPerData(assistant, data);
-        //assistant = this.posController.getCurrentSaleAssistantName();
-        TreeMap<String, Sale> sales = new TreeMap<String, Sale>();
-        sales = salesPerData.get(data);
-        Sale x = sales.get(assistant);
-        System.out.println ("as "+this.posController.getCurrentSaleAssistantName());
-        int total = sales.get(assistant).getTotal();
-        String as = sales.get(assistant).getSaleAssistantName();
-        assertEquals(totalPrice, total);
-        assertEquals(assistant, as);
-    }
 }
