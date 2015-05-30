9daa55f95fba422b715957389fcf3c514b3762b3
GerardDuch <gerard.duch@est.fib.upc.edu>
2015-05-30 13:19:19 +0200

    afegida classe users.java i usersCollection.java, per tenir logins per rols

---
+++ src/main/java/edu/upc/essi/gps/ecommerce/UsersCollection.java
@@ -0,0 +1,12 @@
+package edu.upc.essi.gps.ecommerce;
+
+import java.util.ArrayList;
+
+/**
+ * Created by GerardDuch on 30/05/15.
+ */
+public class UsersCollection {
+    private ArrayList<User> usersList;
+
+
+}
